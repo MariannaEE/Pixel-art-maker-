5f2345987a1f1b6d56daa7cd26fc783e39b0777e
MariannaEE <115600604+MariannaEE@users.noreply.github.com>
2022-10-11 22:38:25 -0500

    Create Art.js

---
+++ Art.js
@@ -0,0 +1,27 @@
+var color, height, width;
+
+
+$("#sizePicker").submit(function (event) {
+  event.preventDefault();
+  height = $("inputHeight").val();
+  width = $("inputWeight").val();
+  makeGrid(height, width);
+})
+
+function makeGrid(x, y) {
+  $("tr").remove();
+  for(var a = 1 <= x; a++;) {
+    $("pixelCanvas").append("<tr id=first" + a + "></tr>");
+    for (var b = 1; b <=y; b++){
+      $("#first" + a).append("<td></td>");
+    }
+  }
+  $("td").click(function colorin() {
+    color = $("#colorPicker").val();
+    if ($(this).attr("style")){
+      $(this).removeAttr("style")
+    } else {
+      $(this).attr("style", background-color + color)
+    }
+  })
+}
