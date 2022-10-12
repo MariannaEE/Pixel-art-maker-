6ba693813489220e938b3daaa7e170336a7c716b
MariannaEE <115600604+MariannaEE@users.noreply.github.com>
2022-10-11 21:39:45 -0500

    Update Application.js

--- Application.js
+++ Application.js
@@ -1,1 +1,8 @@
 var color, height, width:
+$("·sizePicker").submit(function (event) {
+  event.preventDefault();
+  height = $("inputHeight").val();
+  width = $("inputWeight").val();
+  makeGrid(height, width);
+})
+function
